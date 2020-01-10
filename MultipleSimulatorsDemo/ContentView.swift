74daf1ce0fc850ab15de94fc1209197f852a450e
daniel parra <danialepaco@hotmail.com>
2020-01-10 10:34:05 -0500

    first commit

---
+++ MultipleSimulatorsDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MultipleSimulatorsDemo
+//
+//  Created by daniel parra on 10/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is a multi simulator compilation!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
